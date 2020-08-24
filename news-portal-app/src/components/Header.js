2fc8c0c4f0a82cc939476d2187476d995257f90e
Cesar Trevino <cesarhtrevino@gmail.com>
2020-08-24 16:04:58 -0500

    Styling to the News Header

--- news-portal-app/src/components/Header.js
+++ news-portal-app/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Search from "./Search";
+import "./Header.css";
 
 const Header = (props) => {
   return (
-    <div>
-      <h1>Daily Planet</h1>
-      <button>SUSCRIBE NOW</button>
-      <button>LOGIN</button>
+    <div id="news-header-container">
+      <h1 id="news-header-title">Daily Planet</h1>
+      <div id="news-header-button-container">
+        <button>SUSCRIBE NOW</button>
+        <button>LOGIN</button>
+      </div>
     </div>
   );
 };
